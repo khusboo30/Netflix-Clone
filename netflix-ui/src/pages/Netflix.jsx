9f0ed6b6b5d9cfbe160c5b14ad5525b181a69f8b
Khusboo Patel <kkp5601@gmail.com>
2024-04-14 16:43:29 -0400

    Navbar

--- netflix-ui/src/pages/Netflix.jsx
+++ netflix-ui/src/pages/Netflix.jsx
@@ -1,5 +1,22 @@
-import React from "react";
-
+import React, {useState} from "react";
+import Navbar from "../components/Navbar";
+import { useNavigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { firebaseAuth } from "../utils/firebase-config";
 export default function Netflix() {
-  return <div>Netflix</div>;
+
+  const [isScrolled, setIsScrolled] = useState(false);
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+  const navigate = useNavigate();
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
+    if (!currentUser) navigate("/login");
+});
+  return (
+    <div>
+      <Navbar isScrolled={isScrolled} />
+    </div>
+  );
 }
